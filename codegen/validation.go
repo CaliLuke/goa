8009fa713abdd9d35eae4b263efca5676df0353f
CaliLuke <luca.candela@gmail.com>
2026-03-18 15:56:31 -0700

    Apply gofmt after auto-k pre-push hook

--- codegen/validation.go
+++ codegen/validation.go
@@ -202,11 +202,11 @@ func recurseValidationCode(att *expr.AttributeExpr, put expr.UserType, attCtx *A
 					continue
 				}
 				cases = append(cases, map[string]any{
-					"typeTag":      expr.UnionVariantTag(v),
-					"fieldName":    Goify(v.Name, true),
+					"typeTag":       expr.UnionVariantTag(v),
+					"fieldName":     Goify(v.Name, true),
 					"requiresValue": strings.HasPrefix(strings.TrimSpace(val), "if actual != nil {"),
-					"context":      context + ".value",
-					"validation":   val,
+					"context":       context + ".value",
+					"validation":    val,
 				})
 			}
 			if len(cases) > 0 {
